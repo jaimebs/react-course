d9fa82e48762f7412540390355ed575ef3cf8393
Jaime Barbosa <jaime.barbosa@solucaosistemas.net>
2018-08-30 14:40:09 -0300

    Incluido bootstrap

---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class navbar extends Component {
+    render() {
+        return (
+            <div className="Container">
+                <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link" href="#">Sobre</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
