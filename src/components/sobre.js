707e85c5df5abe26293b3c33fa12eed5eaf9ed84
Jaime Barbosa <jaime.barbosa@solucaosistemas.net>
2018-08-30 16:11:47 -0300

    Inclusao do Axios

--- src/components/sobre.js
+++ src/components/sobre.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import API from '../api'
 
 export default class sobre extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>Sobre!</h1>
-      </div>
-    )
-  }
+    state = {
+        comentarios: []
+    }
+
+    componentDidMount() {
+        API.get('/comments').then(resp => {
+            const comentarios = resp.data;
+            this.setState({ comentarios })
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <br />
+                {this.state.comentarios.map(comentario =>
+                    <div className="card" key={comentario.id}>
+                        <h5 className="card-header">{comentario.name}</h5>
+                        <div className="card-body">
+                            <h5 className="card-title">{comentario.email}</h5>
+                            <p className="card-text">{comentario.body}</p>
+                        </div>
+                    </div>
+                )
+                }
+            </div>
+        )
+    }
 }
